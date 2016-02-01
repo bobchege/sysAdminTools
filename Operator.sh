2a526f20ba5c130313e31daed0b76e113e5d2370
aprils3c0nd <bobchege@gmail.com>
2016-02-01 10:24:59 +0300

    New configuration: This code automates operation.

---
+++ Operator.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/expect
+
+###########################################################
+# Author: aprils3c0nd                                     #
+# Date: 02/02/2016                                        #
+# Use: This script is used to log a server and manage     # 
+# log files. It could as well be adapted to run any cmd   #
+###########################################################
+
+set prompt "(%|#|>|\\$) $"
+set user bmuuser
+set password "******"
+set root_password "******"
+
+#for line in $(cat ip.txt); do echo $line; done
+#exit
+set ip [lindex $argv 0];
+
+spawn ssh "$user\@$ip"
+expect ""
+expect "(yes/no)?"
+#send -- "yes"
+#expect ""
+#expect "assword:"
+send "$password\r";
+expect -re $prompt
+send -- "su - root\r"
+expect "assword:"
+send "$root_password\r";
+#expect -re $prompt
+#send -- "cd /var/log/atop\r"
+send -- "ls -al /var/log/atop\r"
+send -- "find /var/log/atop* -mtime +2 -delete;\r"
+send -- "ls -al  /var/log/atop\r"
+#expect -ex "--More--" {send -- " "; exp_continue }
+expect -re $prompt
+send -- "exit\r"
